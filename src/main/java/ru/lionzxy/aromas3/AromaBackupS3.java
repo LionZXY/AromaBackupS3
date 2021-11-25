c66cb3441917517a218fb8362773bcd5f90ea136
LionZXY <nikita@kulikof.ru>
2021-11-25 16:06:52 +0300

    Bump version

--- src/main/java/ru/lionzxy/aromas3/AromaBackupS3.java
+++ src/main/java/ru/lionzxy/aromas3/AromaBackupS3.java
@@ -17,7 +17,7 @@ import org.apache.logging.log4j.Logger;
 public class AromaBackupS3 {
     public static final String MODID = "aromabackups3";
     public static final String NAME = "Aroma Backup S3";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
 
     private static Logger logger;
 
